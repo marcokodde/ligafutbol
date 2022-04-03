0d4a415759483df2f8b40f779e87a95f9dca2d31
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-04-03 16:23:43 -0600

    Tasks:  En el índex se agregan valores falta agregar filtros por  cada uno de los campos padre

--- resources/views/livewire/tasks/list.blade.php
+++ resources/views/livewire/tasks/list.blade.php
@@ -1,5 +1,23 @@
 <tr>
+    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->group->board->title }}</td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->group->title }}</td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->title }}</td>
+    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->deadline }}</td>
+    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
+        @if(App::isLocale('en'))
+            {{ $record->priority->english }}
+        @else
+            {{ $record->priority->spanish }}
+        @endif
+    </td>
+
+    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
+        @if(App::isLocale('en'))
+        {{ $record->status->english }}
+        @else
+            {{ $record->status->spanish }}
+        @endif
+    </td>
+
     @include('common.crud_actions')
 </tr>
