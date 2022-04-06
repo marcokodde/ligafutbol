550a2854e04505436a12860cc399934ff8a00542
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-04-06 16:33:32 -0600

    Seeder de Permission mas completo

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -18,7 +18,15 @@ class PermissionSeeder extends Seeder
 
         $sql= "INSERT INTO permissions (name,slug,english,spanish) VALUES
         ('statuses','statuses.index', 'Statuses List','Listado de Estados'),
-        ('areas','areas.index', 'Areas List','Listado de Areas')";
+        ('priorities','priorities.index', 'Priorities List','Listado de Prioridades'),
+        ('channels','channels.index', 'Channels List','Listado de Canales'),
+        ('positions','positions.index', 'Positions List','Listado de Puestos'),
+        ('departaments','departaments.index', 'Departaments List','Listado de Departamentos'),
+        ('task_types','task_types.index', 'Task Types List','Listado de Tipos de Tarea'),
+        ('clients','clients.index', 'Clients List','Listado de Clientes'),
+        ('boards','boards.index', 'Boards List','Listado de Tableros'),
+        ('groups','groups.index', 'Groups List','Listado de Grupos'),
+        ('tasks','tasks.index', 'Tasks List','Listado de Tareas')";
 
         DB::update ($sql);
     }
