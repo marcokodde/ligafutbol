6ff36a08ebc0fae3a603cf58252b74dc2d1a6cb8
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-04-15 10:54:32 -0600

    Adecuar cantidad de fechas a crear en la prueba

--- routes/pruebas.php
+++ routes/pruebas.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('pruebax',function(){
 
     echo 'FECHAS DE NACIMIENTO ALEATORIAS' . '<br>';
-    for($i=0;$i<=50;$i++){
+    for($i=0;$i<=10;$i++){
         $fecha_final = new Carbon('2015-12-31');
-        echo $dias .'=' . $fecha_final->subDays(random_int(0, 2550))->format('Y/m/d') . '<br>';
+        echo $fecha_final->subDays(random_int(0, 2550))->format('Y/m/d') . '<br>';
      }
 
 
