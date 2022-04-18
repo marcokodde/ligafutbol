cb25f1f052d168868143b7ed843f4980c9e7cb6d
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-04-18 12:03:58 -0600

    Etiquetas de traducción

---
+++ database/seeders/insert_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`name`, `email`, `phone`, `email_verified_at`, `password`, `active`) VALUES
+('Nombre Usuario', 'correo@dominio.com', '0000000000',NULL, '$2y$10$XtjvN.8T3nyzmGP6hZtKpuE9JXPAGB888CIc/dyc0ORYF29dHO/xq', 0);
+COMMIT;
+
+Nota: '$2y$10$XtjvN.8T3nyzmGP6hZtKpuE9JXPAGB888CIc/dyc0ORYF29dHO/xq' = 'password'
