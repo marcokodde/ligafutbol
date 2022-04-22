3bf9e815ad2e3fb5741ae3345acb79431de30676
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-04-22 09:42:11 -0600

    Cambio en listado de configuración

--- resources/views/livewire/settings/table.blade.php
+++ resources/views/livewire/settings/table.blade.php
@@ -1,9 +1,12 @@
 <thead>
     <tr class="lg:hover:bg-gray-100 lg:bg-gray-50">
-        <th class="px-4 py-2 w-80">{{__("Name")}}</th>
-        <th class="px-4 py-2 w-80">{{__("Players by Team")}}</th>
-        <th class="px-4 py-2 w-80">{{__("Players Only Enabled Teams")}}</th>
-        <th class="px-4 py-2 w-80">{{__("Players Only Enabled Teams")}}</th>
+        <th class="px-4 py-2 w-80" rowspan="2">{{__("Name")}}</th>
+        <th class="px-4 py-2 w-80" rowspan="2">{{__("Players by Team")}}</th>
+        <th class="px-4 py-2 text-center" colspan="3">{{__("Assing Only Enabled Teams")}}</th>
+    </tr>
+    <tr class="lg:hover:bg-gray-100 lg:bg-gray-50">
+        <th class="px-4 py-2 w-20">{{__("Players")}}</th>
+        <th class="px-4 py-2 w-20">{{__("Coaches")}}</th>
          <th colspan="3" class="px-4 py-2 text-center">{{__("Actions")}}</th>
     </tr>
 </thead>
