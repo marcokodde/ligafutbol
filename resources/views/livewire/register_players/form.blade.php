e5affb750f895e41597dc682694bb6ff4cf0fa5f
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-01 11:34:43 -0600

    Register Players: Formulario ejemplo antes de pasarlos a componentes hijos

--- resources/views/livewire/register_players/form.blade.php
+++ resources/views/livewire/register_players/form.blade.php
@@ -62,16 +62,101 @@
             </div>
 
             @if($team_id)
-                <div class="grid grid-cols-4">
-                    Se ha seleccionado equipo
+                <div class="mt-5 border-2 border-green-400">
+                    <div class="sm:px-0 mx-auto text-center items-center">
+                        <label class="text-center text-2xl font-bold mb-10">JUGADORES INSCRITOS EN EL EQUIPO</label>
+                    </div>
+                    <div class="sm:px-0 mx-auto text-center items-center">
+                        <table>
+                            <thead>
+                                <th>{{__('First Name')}}</th>
+                                <th>{{__('Last Name')}}</th>
+                                <th>{{__('Gender')}}</th>
+                                <th>{{__('Birthday')}}</th>
+                            </thead>
+                            <tr>
+                                <td>Pedro</td>
+                                <td>Páramo</td>
+                                <td>Hombre</td>
+                                <td>Jul-18-1955</td>
+                                <td>Quitar</td>
+                            </tr>
+                            <tr>
+                                <td>Benito</td>
+                                <td>Juárez</td>
+                                <td>Hombre</td>
+                                <td>Mar-21-1806</td>
+                                <td>Quitar</td>
+                            </tr>
+                            <tr class=" bg-pink-300">
+                                <td>Frida</td>
+                                <td>Kahlo</td>
+                                <td>Mujer</td>
+                                <td>Jul-06-1907</td>
+                                <td>Quitar</td>
+                            </tr>
+
+                        </table>
+
+                    </div>
                 </div>
+
+                <div class=" mt-32 border-2 border-red-400 w-full">
+                    <div class="MT-10 sm:px-0 mx-auto text-center items-center">
+                        <label class="text-center text-2xl font-bold">FORMULARIO PARA AGREGAR JUGADORES</label>
+                    </div>
+
+                    <div class="sm:px-0 mx-auto text-center items-center">
+                        <table>
+                            <thead>
+                                <th>{{__('First Name')}}</th>
+                                <th>{{__('Last Name')}}</th>
+                                <th>{{__('Gender')}}</th>
+                                <th>{{__('Birthday')}}</th>
+                            </thead>
+                            <tr>
+                                <td><input type="text" name="" id=""  placeholder="{{__('First Name')}}"></td>
+                                <td><input type="text" name="" id=""  placeholder="{{__('Last Name')}}"></td>
+                                <td>
+                                    <input type="radio"
+                                            wire:model="gender"
+                                            class="form-check-input h-4 w-4 bg-blue-500"
+                                            value="Male">
+
+                                    <label class="text-blue-500">{{ __('M') }}</label>
+
+                                    <input type="radio"
+                                            wire:model="gender"
+                                            class="form-check-input h-4 w-4"
+                                            value="Female"
+                                    >
+
+                                    <label class="text-pink-500">{{ __('F') }}</label>
+                                </td>
+                                <td>
+                                    <input type="date"
+                                    wire:model="birthday"
+                                    min=""
+                                    max=""
+                                    placeholder="{{__("Birthday")}}"
+                                    class=" w-1/8"
+                                >
+
+
+                                </td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+
             @endif
 
         </div>
 
     </div>
 
-    @if(!$finish)
+    {{-- @if(!$finish)
         <div class="row mt-5">
             <div class="flex text-center items-center justify-center bg-gray-50  sm:px-6">
                 <button wire:click="review_data"
@@ -81,4 +166,4 @@
                 </button>
             </div>
         </div>
-    @endif
+    @endif --}}
