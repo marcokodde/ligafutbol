e55c3da1bc4b40da8f5779ea262345b52908698a
marcokodde <marco@koddeplus.com>
2022-05-02 18:50:39 -0500

    Agregando puntos de quiebre en vista register players

--- resources/views/livewire/register_players/form.blade.php
+++ resources/views/livewire/register_players/form.blade.php
@@ -8,21 +8,21 @@
     <hr class="border-2 border-gray-500">
 </div>
 
-<div class="grid lg:grid-cols-3 lg:gap-4 sm:grid-cols-1 sm:gap-1">
-        <div></div>
-        <div>
-            @include('livewire.register_players.select_category_and_team')
-            @if($team_id)
-                @if($team->players->count() > 0)
-                    @livewire('show-players-team',['team' => $this->team])
-                @endif
+<div class="grid lg:grid-cols-3 lg:gap-4 md:grid-cols-1 md:gap-2 sm:grid-cols-1 sm:gap-1">
+    <div></div>
+    <div>
+        @include('livewire.register_players.select_category_and_team')
+        @if($team_id)
+            @if($team->players->count() > 0)
+                @livewire('show-players-team',['team' => $this->team])
             @endif
-        </div>
+        @endif
     </div>
+</div>
 
-<div class="relative font-medium">
-    <div class="flex gap-2">
-        <div class="absolute top-0 left-0">
+<div class="relative font-medium mt-4">
+    <div class="flex gap-2 justify-center items-center">
+        <div class="absolute top-0 mx-auto items-center justify-center">
             @if($team_id)
                 @if($team->players->count() < $general_settings->max_players_by_team )
                     @livewire('add-player',['team' => $this->team,'user' => $user])
