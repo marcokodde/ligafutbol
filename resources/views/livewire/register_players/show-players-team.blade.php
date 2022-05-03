38f45ebeac32e86e55a2c0ad53b8e4146c56a079
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-03 17:58:16 -0600

    Regsistro de Jugadores: Dejar sólo nombre en lista de jugadores inscritos

--- resources/views/livewire/register_players/show-players-team.blade.php
+++ resources/views/livewire/register_players/show-players-team.blade.php
@@ -6,15 +6,16 @@
     </div>
     <table class="border-collapse w-full">
         <thead>
-            <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('First Name')}}</th>
-            <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Last Name')}}</th>
+            <th colspan="2" class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Name')}}</th>
+            {{-- <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Last Name')}}</th>
             <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Gender')}}</th>
-            <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Birthday')}}</th>
+            <th class="border-b font-medium border text-gray-700 lg:table-cell font-pop">{{__('Birthday')}}</th> --}}
         </thead>
         <body>
             @foreach($team->players as $team_player)
                 <tr>
-                    <td>
+                    <td>{{$team_player->fullName}}</td>
+                    {{-- <td>
                         <span class="lg:hidden absolute top-4 left-0 px-2 py-1 text-xs font-bold">First Name</span>
                         {{$team_player->first_name}}</td>
                     <td>
@@ -26,7 +27,12 @@
                     <td>
                         <span class="lg:hidden absolute top-40 left-0 px-2 py-1 text-xs font-bold">Birthday</span>
                         {{date("F j Y", strtotime($team_player->birthday))}}</td>
-                    <td> <button wire:click="removePlayer({{ $team_player->pivot->player_id }})" class="bg-red-500 hover:bg-red-900 text-white font-bold py-1 px-2 rounded-lg text-center">{{__("Remove")}}</button></td>
+                     --}}
+                    <td>
+                         <button wire:click="removePlayer({{ $team_player->pivot->player_id }})" class="bg-red-500 hover:bg-red-900 text-white font-bold py-1 px-2 rounded-lg text-center">
+                            {{__("Remove")}}
+                        </button>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
