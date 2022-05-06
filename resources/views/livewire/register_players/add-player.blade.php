dacc95ba421cced5566a28a12e2e14dcb0672d26
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-06 10:21:38 -0600

    Mensajes de error

--- resources/views/livewire/register_players/add-player.blade.php
+++ resources/views/livewire/register_players/add-player.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div wire:loading.delay wire:target="addingPlayer" class="flex mx-auto justify-center items-center">
-        <button type="button" class="justify-center items-center text-center text-2xl font-pop font-semibold px-2 py-2  leading-6 shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled="">
-            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <div wire:loading.delay wire:target="addingPlayer" class="flex items-center justify-center mx-auto">
+        <button type="button" class="items-center justify-center px-2 py-2 text-2xl font-semibold leading-6 text-center text-white transition duration-150 ease-in-out bg-indigo-500 rounded-md shadow cursor-not-allowed font-pop hover:bg-indigo-400" disabled="">
+            <svg class="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
@@ -10,32 +10,41 @@
     </div>
     <div class="flex flex-wrap mt-2">
         <div>
-            <label class="block font-pop text-base">{{__('First Name')}} @error('first_name') <label class="text-red-500 text-xl inline" >*</label>@enderror</label>
+            <label class="block text-base font-pop">{{__('First Name')}} @error('first_name') <label class="inline text-xl text-red-500" >*</label>@enderror</label>
             <input type="text"
                 wire:model="first_name"
                 maxlength="30"
                 minlength="5"
                 placeholder="{{__('First Name')}}"
                 class="block  @error('first_name')  border-red-600 border-collapse border-2 @enderror">
+                @error('first_name')
+                    <span class="bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900">
+                        {{$message}}
+                    </span>
+                @enderror
         </div>
 
         <div class="lg:ml-2 sm:ml-0">
-            <label class="block font-pop text-base">{{__('Last Name')}} @error('last_name') <label class="text-red-500 text-xl inline" >*</label>@enderror</label>
+            <label class="block text-base font-pop">{{__('Last Name')}} @error('last_name') <label class="inline text-xl text-red-500" >*</label>@enderror</label>
             <input type="text"
                 wire:model="last_name"
                 placeholder="{{__('Last Name')}}"
                 class="block  @error('last_name')  border-red-600 border-collapse border-2 @enderror">
-                
+            @error('last_name')
+                <span class="bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900">
+                    {{$message}}
+                </span>
+            @enderror
         </div>
         <div class="ml-1">
-            <label class="block font-pop text-base">{{__('Gender')}}@error('gender') <label class="text-red-500 text-xl" >*</label>@enderror</label>
+            <label class="block text-base font-pop">{{__('Gender')}}@error('gender') <label class="text-xl text-red-500" >*</label>@enderror</label>
             <div class="flex justify-between @error('gender') border-red-600 border-collapse border-2 @enderror">
-                <div class="mt-1 px-2">
+                <div class="px-2 mt-1">
                     <label class="text-blue-500">{{ __('Boy') }}</label>
                     <input type="radio"
                     wire:model="gender"
                     wire:change="birthday_limits"
-                    class="form-check-input h-4 w-4"
+                    class="w-4 h-4 form-check-input"
                     value="Male">
                 </div>
 
@@ -44,26 +53,35 @@
                     <input type="radio"
                     wire:model="gender"
                     wire:change="birthday_limits"
-                    class="form-check-input h-4 w-4 rounded-full"
+                    class="w-4 h-4 rounded-full form-check-input"
                     value="Female">
                 </div>
             </div>
+            @error('gender')
+                <span class="bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900">
+                    {{$message}}
+                </span>
+             @enderror
         </div>
         <div>
-            <label class="block font-pop text-base">{{__('Birthday')}}</label>
+            <label class="block text-base font-pop">{{__('Birthday')}}</label>
             <input type="date"
                 wire:model="birthday"
                 min="{{$birthday_min}}"
                 max="{{$birthday_max}}"
                 placeholder="{{__("Birthday")}}"
                 class="block ml-1 w-11/12 @error('birthday')  border-red-600 border-collapse border-2 @enderror">
-                @error('birthday') <label class="text-red-500 text-sm" >{{$message}}</label>@enderror
-            </div>
+                @error('birthday')
+                    <label class="bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-900">
+                        {{$message}}
+                    </label>
+                @enderror
+        </div>
 
         <div class="ml-2">
-            <label class="block font-pop text-base">&nbsp;</label>
+            <label class="block text-base font-pop">&nbsp;</label>
             <button wire:click="addingPlayer"
-                class="block bg-green-500 px-4 py-2 rounded-lg  text-black hover:text-white">
+                class="block px-4 py-2 text-black bg-green-500 rounded-lg hover:text-white">
                 {{ __('Add')}}
             </button>
         </div>
