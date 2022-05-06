963d2da7dc9d1326c7e7d672de1a0030942b2cb3
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-06 02:10:30 -0600

    AddPlayer: Se elminan variables innecesarias

--- resources/views/livewire/register_players/add-player.blade.php
+++ resources/views/livewire/register_players/add-player.blade.php
@@ -1,65 +1,72 @@
-<div class="flex flex-wrap mt-2">
-    <div>
-        <label class="block font-pop text-base">{{__('First Name')}}</label>
-        <input type="text"
-                wire:model="first_name"
-                maxlength="30"
-                minlength="5"
-                placeholder="{{__('First Name')}}"
-                class="block  @error('first_name')  bg-red-500  @enderror"
-        >
-
+<div>
+    <div wire:loading.flex wire:target="addingPlayer" class=" text-4xl font-bold bg-green-300 border-8 border-blue-600">
+        {{__('Adding Player at Team')}}
     </div>
+    <div class="flex flex-wrap mt-2">
 
-    <div class="lg:ml-2 sm:ml-0">
-        <label class="block font-pop text-base">{{__('Last Name')}}</label>
-        <input type="text"
-                wire:model="last_name"
-                placeholder="{{__('Last Name')}}"
-                class="block  @error('last_name')  bg-red-500  @enderror"
-        >
-
-    </div>
         <div>
-            <label class="block font-pop text-base">{{__('Birthday')}}</label>
-            <input type="date"
-                    wire:model="birthday"
-                    min="{{$birthday_min}}"
-                    max="{{$birthday_max}}"
-                    placeholder="{{__("Birthday")}}"
-                    class="block  w-11/12 @error('birthday')  bg-red-500  @enderror"
+            <label class="block font-pop text-base">{{__('First Name')}}</label>
+            <input type="text"
+                    wire:model="first_name"
+                    maxlength="30"
+                    minlength="5"
+                    placeholder="{{__('First Name')}}"
+                    class="block  @error('first_name')  bg-red-500  @enderror"
             >
-        </div>
-    <div>
-        <label class="block font-pop text-base">{{__('Gender')}}</label>
-        <div class="flex justify-between @error('gender') bg-red-500 @enderror">
-            <div class="mt-1 px-2">
-                <label class="text-blue-500">{{ __('Boy') }}</label>
-                <input type="radio"
-                wire:model="gender"
-                wire:change="birthday_limits"
-                class="form-check-input h-4 w-4"
-                value="Male">
-            </div>
 
-            <div class="mt-1 ml-2">
-                <label class="text-pink-500">{{ __('Girl') }}</label>
-                <input type="radio"
-                wire:model="gender"
-                wire:change="birthday_limits"
-                class="form-check-input h-4 w-4 rounded-full"
-                value="Female">
-            </div>
         </div>
 
-    </div>
-    <div class="ml-2">
+        <div class="lg:ml-2 sm:ml-0">
+            <label class="block font-pop text-base">{{__('Last Name')}}</label>
+            <input type="text"
+                    wire:model="last_name"
+                    placeholder="{{__('Last Name')}}"
+                    class="block  @error('last_name')  bg-red-500  @enderror"
+            >
 
-        <label class="block font-pop text-base">&nbsp;</label>
-        <button wire:click="addingPlayer"
-                class="block bg-green-500 px-4 py-2 rounded-lg  text-black hover:text-white"
-        >
-            {{ __('Add')}}
-        </button>
+        </div>
+            <div>
+                <label class="block font-pop text-base">{{__('Birthday')}}</label>
+                <input type="date"
+                        wire:model="birthday"
+                        min="{{$birthday_min}}"
+                        max="{{$birthday_max}}"
+                        placeholder="{{__("Birthday")}}"
+                        class="block  w-11/12 @error('birthday')  bg-red-500  @enderror"
+                >
+            </div>
+        <div>
+            <label class="block font-pop text-base">{{__('Gender')}}</label>
+            <div class="flex justify-between @error('gender') bg-red-500 @enderror">
+                <div class="mt-1 px-2">
+                    <label class="text-blue-500">{{ __('Boy') }}</label>
+                    <input type="radio"
+                    wire:model="gender"
+                    wire:change="birthday_limits"
+                    class="form-check-input h-4 w-4"
+                    value="Male">
+                </div>
+
+                <div class="mt-1 ml-2">
+                    <label class="text-pink-500">{{ __('Girl') }}</label>
+                    <input type="radio"
+                    wire:model="gender"
+                    wire:change="birthday_limits"
+                    class="form-check-input h-4 w-4 rounded-full"
+                    value="Female">
+                </div>
+            </div>
+
+        </div>
+        <div class="ml-2">
+
+            <label class="block font-pop text-base">&nbsp;</label>
+            <button wire:click="addingPlayer"
+                    class="block bg-green-500 px-4 py-2 rounded-lg  text-black hover:text-white"
+            >
+                {{ __('Add')}}
+            </button>
+
+        </div>
     </div>
-</div>
+</div
