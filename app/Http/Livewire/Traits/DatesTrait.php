6adbb2a3e1b0ba5307a5f3b0d2650166e9d3e52a
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-13 11:01:16 -0600

    Traita para gestionar nombres de meses ver ruta de test_controller

---
+++ app/Http/Livewire/Traits/DatesTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire\Traits;
+
+use Carbon\Carbon;
+
+
+
+trait DatesTrait {
+
+    public $short_months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    public $large_months = ['January','Februart','March','April','May','June','July','August','September','October','November','December'];
+    PUBLIC $days_by_month= [31,28,31,30,31,30,31,31,30,31,30,31];
+    public $max_days_by_month;
+
+    public $date_day,$date_month,$date_year;
+    public $date_full_date;
+
+    public function createDate(){
+        if($this->date_day && $this->date_month && $this->date_year){
+            $this->date_full_date = $this->date_year . '-' . $this->date_month . '-' . $this->date_day;
+        }
+    }
+
+}
--- resources/views/livewire/test-controller.blade.php
+++ resources/views/livewire/test-controller.blade.php
@@ -1,14 +1,58 @@
 <div>
-   El total es: {{ $total}}
+    @if($month)
+        <p>Mes: {{ $month . '=' . $short_months[$month-1] . ' =>' .  $large_months[$month-1]}} </p>
+        <p>Dia: {{ $day}}</p>
+        <p>Año: {{ $year}}</p>
+    @endif
+
 
     <div class="flex">
-        <button wire:click="sumar" class="button green rounded-lg hover:text-black">
-            {{__("Sumar")}}
-        </button>
+        <table>
+            <thead class="text-center">
+                <th>{{__('Month')}}</th>
+                <th>{{__('Day')}}</th>
+                <th>{{__('Year')}}</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <select wire:model="month">
+                            <option value="">{{__('Month')}}</option>
+                            @foreach($short_months as $month_sel)
+                                <option value="{{$loop->index+1}}">{{__($month_sel)}}</option>
+                            @endforeach
+                        </select>
+                    </td>
+                    {{-- Día según mes elegido --}}
 
-        <button wire:click="restar" class="button red rounded-lg hover:text-black">
-            {{__("Restar")}}
-        </button>
+                    <td>
+                        <select wire:model="day">
+                            <option value="">{{__('Day')}}</option>
+                            @if($month)
+                                @for($i=1;$i<=$days_by_month[$month-1];$i++)
+                                    <option value="{{str_pad($i, 2, "0", STR_PAD_LEFT)}}">{{str_pad($i, 2, "0", STR_PAD_LEFT)}}</option>
+                                @endfor
+                            @endif
+                        </select>
+                    </td>
+                    <td>
+                        <div class="form-group lg:ml-2 sm:ml-0 mb-2 mt-2">
+                            @php $year = date('Y') @endphp
+                            <select wire:model="year" class='lg:w-40'>
+                                <option value="">{{__('Year')}}</option>
+                                @for($i=$year;$i<=$year+10;$i++)
+                                    <option value="{{str_pad($i, 4, "0", STR_PAD_LEFT)}}">{{str_pad($i, 4, "0", STR_PAD_LEFT)}}</option>
+                                @endfor
+                            </select>
+                        </div>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
+
+
+
     </div>
 
 </div>
