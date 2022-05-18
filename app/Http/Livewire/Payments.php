5fa4e1a1c77c5cd835e94172d8ffb6af85bb1566
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-18 15:25:49 -0600

    Se estaba mandando dos veces el correo

--- app/Http/Livewire/Payments.php
+++ app/Http/Livewire/Payments.php
@@ -127,7 +127,6 @@ class Payments extends Component
                     $this->create_Teamcategory($request, $payment);
                     $this->sendMail($request);
                 }
-                $this->sendMail($request);
 
             } catch (\Throwable $exception) {
 
@@ -220,7 +219,7 @@ class Payments extends Component
 			'name'      => $this->fullname,
 			'email'     => $this->email,
             'phone'     => $this->phone,
-            'password' => Hash::make($this->phone)
+            'password'  => Hash::make($this->phone)
         ]);
         $coach = Coach::updateOrCreate(['id' => $this->record_id], [
             'name'      => $this->fullname,
