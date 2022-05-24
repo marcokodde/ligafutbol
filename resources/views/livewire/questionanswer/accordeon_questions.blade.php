f8d4ea3ac5fb9099f9b1e16781eeecca458a83fa
marcokodde <marco@koddeplus.com>
2022-05-24 09:47:44 -0500

    Agregando estilo a input de busqueda

--- resources/views/livewire/questionanswer/accordeon_questions.blade.php
+++ resources/views/livewire/questionanswer/accordeon_questions.blade.php
@@ -5,7 +5,7 @@
         <input type="text"
         wire:model="search"
         placeholder="{{__($search_label)}}"
-        class="mb-4 w-1/3 shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="mb-4 w-1/4 shadow appearance-none border rounded-lg  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         >
     </div>
   {{--     --}}
