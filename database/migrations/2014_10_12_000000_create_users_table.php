ad0f98a26a6b2deae87974c139296d22e6a50de5
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-05-27 11:39:13 -0600

    Users: Migración con UNIQUE en email y phone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone',10)->nullable();
+            $table->string('email')->unique();
+            $table->string('phone',10)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
