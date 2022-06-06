125f4b4bb3c645dc358c44b1a967370dcef2aaba
marcokodde <marco@koddeplus.com>
2022-06-06 12:39:11 -0500

    Agregando validacion en botones steps

--- resources/views/livewire/payments/buttons_steps.blade.php
+++ resources/views/livewire/payments/buttons_steps.blade.php
@@ -23,17 +23,20 @@
             {{__("Confirm Payment")}}
         </button>
     @elseif($currentPage === 2)
-        @if ($total_teams && !Auth::user()->isSupport() && !Auth::user()->isAdmin())
+        @if ($total_teams && !Auth::user())
             <button wire:click="goToNextPage" type="button" class="button blue mx-2 px-8 py-4 mt-4 font-semibold  rounded-lg hover:text-black">
                 {{__("Next")}}
             </button>
-        @else($total_teams && Auth::user()->isSupport() || Auth::user()->isAdmin())
-            <button wire:click="register_by_admin"
-                type="button"
-                class="block button blue rounded-lg mx-2 px-8 py-4 mt-4 font-semibold hover:text-black">
-                {{__("Next")}}
-            </button>
         @endif
+        @auth
+            @if ($total_teams )
+                <button wire:click="register_by_admin"
+                    type="button"
+                    class="block button blue rounded-lg mx-2 px-8 py-4 mt-4 font-semibold hover:text-black">
+                    {{__("Next")}}
+                </button>
+            @endif
+        @endauth
     @else
         @if ($fullname && $phone && $email && $same_phone_and_email && $currentPage === 1)
             <button wire:click="goToNextPage_and_create_user_without"
