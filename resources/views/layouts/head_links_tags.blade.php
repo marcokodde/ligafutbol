ca1c7abc4035efbe8331515ef389e5fd6cf29cec
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-06-27 15:50:56 -0600

    Incorporar FA AWASOME icons

--- resources/views/layouts/head_links_tags.blade.php
+++ resources/views/layouts/head_links_tags.blade.php
@@ -4,6 +4,6 @@
         <link rel="icon" type="image/png" sizes="32x32" href="{{asset('admin-one/dist/favicon-32x32.png')}}"/>
         <link rel="icon" type="image/png" sizes="32x32" href="{{asset('admin-one/dist/favicon-16x16.png')}}"/>
         <link rel="mask-icon" href="{{asset('admin-one/dist/safari-pinned-tab.svg')}}" color="#00b4b6"/>
-
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
         <!-- Tailwind CSS -->
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
--- resources/views/layouts/menus/admin.blade.php
+++ resources/views/layouts/menus/admin.blade.php
@@ -7,55 +7,64 @@
     <ul>
         <li>
             <a href="{{url('statuses')}}">
-                <span>{{__('Status')}}</span>
+                <span class="icon"><i class="mdi mdi-format-list-bulleted"></i></span>
+                <span>{{__('Statuses')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('users')}}">
-            <span>{{__('users')}}</span>
+            <span class="icon"><i class="mdi mdi-account"></i></span>
+            <span>{{__('Users')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('permission')}}">
-            <span>{{__('Permissions')}}</span>
+                <span class="icon"><i class="mdi mdi-account-cash-outline"></i></span>
+                <span>{{__('Permissions')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('role')}}">
-            <span>{{__('Roles')}}</span>
+                <span class="icon"><i class="mdi mdi-account-convert-outline"></i></span>
+                <span>{{__('Roles')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('categories')}}">
-            <span>{{__('Categories')}}</span>
+                <span class="icon"><i class="mdi mdi-calendar"></i></span>
+                <span>{{__('Categories')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('costs-by-team')}}">
-            <span>{{__('Costs By Team')}}</span>
+                <span class="icon"><i class="fa fa-money-bill"></i></span>
+                <span>{{__('Costs By Team')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('settings')}}">
-            <span>{{__('Settings')}}</span>
+                <span class="icon"><i class=" fas fa-bezier-curve"></i></span>
+                <span>{{__('Settings')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('email_notifications')}}">
-            <span>{{__('Notifications')}}</span>
+                <span class="icon"><i class="mdi mdi-bell-ring"></i></span>
+                <span>{{__('Notifications')}}</span>
             </a>
         </li>
 
         <li>
             <a href="{{url('promoters')}}">
-            <span>{{__('Promoters')}}</span>
+                <span class="icon"><i class="fas fa-people-carry"></i></span>
+                <span>{{__('Promoters')}}</span>
             </a>
         </li>
 
