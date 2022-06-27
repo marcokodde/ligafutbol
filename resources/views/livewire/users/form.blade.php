c934ce42e972eb459d95a5824ccdd8a0b779d2a4
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-06-27 16:29:32 -0600

    Seleccionar Role en Alta de Usuario: Ya no ejecuta el change

--- resources/views/livewire/users/form.blade.php
+++ resources/views/livewire/users/form.blade.php
@@ -27,11 +27,12 @@
                                 <input type="password" wire:model="password"  maxlength="50"  placeholder="{{__("Password")}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                                 @error('password') <span class="text-red-500">{{ $message }}</span>@enderror
                             </div>
-                           
+
                             <label class="block text-gray-700 text-sm font-bold mb-2 text-left">{{__("Role")}}</label>
-                            <select wire:model="role_id" wire:change="update_role_id()" class="block w-full bg-white border border-white-200 text-gray-700 py-2 px-4 pr-8 mb-3 rounded leading-tight focus:outline-none focus:shadow-outline">
+                            <select wire:model="role_id"
+                                    class="block w-full bg-white border border-white-200 text-gray-700 py-2 px-4 pr-8 mb-3 rounded leading-tight focus:outline-none focus:shadow-outline">
                                 <option>{{__("Select Role")}}</option>
-                                
+
                                 @foreach($roles as $role)
                                     <option value="{{$role->id}}">
                                         @if(App::isLocale('en'))
