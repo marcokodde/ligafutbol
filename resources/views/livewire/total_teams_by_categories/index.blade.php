3580d7f4f14dceedd36fc2ebd15dbcb33940df73
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-07-04 13:39:49 -0600

    Dejar abierto el acordeón en consulta de equipos x categorías

--- resources/views/livewire/total_teams_by_categories/index.blade.php
+++ resources/views/livewire/total_teams_by_categories/index.blade.php
@@ -4,7 +4,7 @@
         <div class="overflow-hidden border-t" wire:key="foo{{ $record->id }}" data-accordion="open" >
             <label>
                 <input class="absolute opacity-0 peer" type="checkbox" name="" id="{{$record->id}}">
-                <p class="cursor-pointer p-5 inline-block w-11/12 font-pop font-semibold">{{$record->category->name}}
+                <p class="inline-block w-11/12 p-5 font-semibold cursor-pointer font-pop">{{$record->category->name}}
                     @if($record->category->teams->count())
                         <label class="font-semibold font-pop">
                             , {{ __('Registered') . ':' . $record->category->teams->count()}}
@@ -12,10 +12,11 @@
                     @endif
                 </p>
                 <svg xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6 cursor-pointer inline-block float-right mr-20 mt-5 border-2 rounded-full peer-checked:rotate-45 peer-checked:bg-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    class="inline-block float-right w-6 h-6 mt-5 mr-20 border-2 rounded-full cursor-pointer peer-checked:rotate-45 peer-checked:bg-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
                 </svg>
-                <div class="bg-gray-300 peer-checked:max-h-0 max-h-screen rounded-lg">
+                <div class="max-h-screen bg-gray-300 rounded-lg peer-checked:max-h-0">
+                <div class="bg-gray-300 rounded-lg">
                     @if ($record->category->teams->count())
                         @foreach ($record->category->teams as $team)
                             <p class="p-5 font-pop">
