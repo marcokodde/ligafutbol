945f9410d84c4c8baa565bace4ffdf835f8de987
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-07-12 10:40:13 -0600

    Consulta: Usuarios-Categoría-Equipo-Ciudad - Estado-Pago

--- database/operacione base de datos/equipos x categoría.sql
+++ database/operacione base de datos/equipos x categoría.sql
@@ -13,3 +13,13 @@ WHERE cat.id = te.category_id
   AND zc.zipcode = te.zipcode
   AND us.accept_responsibilities IS NULL
 ORDER BY us.name,cat.name
+
+//
+SELECT us.name, us.phone, cat.name,te.name,zip.town,zip.state,pay.amount/pay.source as Pagado
+FROM users us
+LEFT JOIN teams as 	te ON us.id = te.user_id
+LEFT JOIN categories  	cat ON cat.id = te.category_id
+LEFT JOIN zipcodes  	zip ON zip.zipcode = te.zipcode
+LEFT JOIN payments 		pay ON pay.id = te.payment_id
+WHERE us.id NOT IN (1,2,3)
+ORDER BY us.name,cat.name,te.name
