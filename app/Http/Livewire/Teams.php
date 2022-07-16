cf43ef9ca4d363fd9ee648ac601a999b03050a78
marcokodde <marco@koddeplus.com>
2022-07-15 20:05:15 -0500

    Agregando validacion para teams

--- app/Http/Livewire/Teams.php
+++ app/Http/Livewire/Teams.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Livewire\Traits\CrudTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Teams extends Component {
+class Teams extends Component
+{
     use AuthorizesRequests;
     use WithPagination;
     use CrudTrait;
@@ -30,132 +31,142 @@ class Teams extends Component {
     {
         //$this->authorize('hasaccess', 'Teams.index');
         $this->manage_title = __('Manage') . ' ' . __('Teams');
-        $this->search_label = "Team Name";
+        $this->search_label = 'Team Name';
         $this->view_form = 'livewire.teams.form';
         $this->view_table = 'livewire.teams.table';
-        $this->view_list  = 'livewire.teams.list';
-        $this->view_search  = 'livewire.teams.search';
-        $this->categories = Category::Active()->orderBy('date_from')->get();
+        $this->view_list = 'livewire.teams.list';
+        $this->view_search = 'livewire.teams.search';
+        $this->categories = Category::Active()
+            ->orderBy('date_from')
+            ->get();
+        if (Auth::user()->IsAdmin()) {
+            $this->allow_create = false;
+        }
     }
 
-
     /*+----------------------------------------------+
-	| Presenta formulario filtrando la búsqueda    |
-	+----------------------------------------------+
-	 */
+ | Presenta formulario filtrando la búsqueda    |
+ +----------------------------------------------+
+ */
 
-	public function render() {
-        $this->create_button_label =  $this->record_id ?    __('Update') . ' ' . __('Team')
-                                                        : __('Create') . ' ' . __('Team');
+    public function render()
+    {
+        $this->create_button_label = $this->record_id ? __('Update') . ' ' . __('Team') : __('Create') . ' ' . __('Team');
 
         $searchTerm = '%' . $this->search . '%';
         if (Auth::user()->IsAdmin()) {
             return view('livewire.teams.index', [
                 'records' => Team::Name($searchTerm)
-                            ->ByCategory($this->category_id)
-                            ->paginate($this->pagination),
+                    ->ByCategory($this->category_id)
+                    ->paginate($this->pagination),
             ]);
         }
         return view('livewire.index', [
-            'records' => Team::UserId()->Name($searchTerm)->paginate($this->pagination),
+            'records' => Team::UserId()
+                ->Name($searchTerm)
+                ->paginate($this->pagination),
         ]);
-	}
+    }
 
-   /*+----------------------+
-	| Inicializa variables  |
-	+-----------------------+
+    /*+----------------------+
+ | Inicializa variables  |
+ +-----------------------+
     */
 
-	private function resetInputFields() {
+    private function resetInputFields()
+    {
         $this->record_id = null;
         $this->record = null;
-        $this->reset(['name','category_id','zipcode','active', 'enabled']);
-	}
+        $this->reset(['name', 'category_id', 'zipcode', 'active', 'enabled']);
+    }
 
     /*+---------------------------------------------+
     | Valida, crea o actualiza según corresponda  |
     +---------------------------------------------+
     */
 
-	public function store() {
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required|min:3|max:50',
+            'category_id' => 'required|not_in:Elegir|not_in:Choose|exists:categories,id',
+            'zipcode' => 'required|min:3|max:5|exists:zipcodes,zipcode',
+        ]);
 
-		$this->validate([
-            'name'          => 'required|min:3|max:50',
-            'category_id'   => 'required|not_in:Elegir|not_in:Choose|exists:categories,id',
-            'zipcode'       => 'required|min:3|max:5|exists:zipcodes,zipcode',
-		]);
-
-		Team::updateOrCreate(['id' => $this->record_id], [
-            'name'          => $this->name,
-			'category_id'   => $this->category_id,
-            'zipcode'       => $this->zipcode,
-            'user_id'       => Auth::user()->id,
-            'active'        => $this->active ? 1 : 0,
-            'enabled'       => $this->enabled ? 1 : 0
-		]);
+        Team::updateOrCreate(
+            ['id' => $this->record_id],
+            [
+                'name' => $this->name,
+                'category_id' => $this->category_id,
+                'zipcode' => $this->zipcode,
+                'user_id' => Auth::user()->id,
+                'active' => $this->active ? 1 : 0,
+                'enabled' => $this->enabled ? 1 : 0,
+            ],
+        );
 
         //TODO: Probar elAgregar para que sume los equipos por categoría. (lugares disponibles)
-        if(!$this->record_id){
+        if (!$this->record_id) {
             TeamCategory::create([
-                'user_id'     =>  Auth::user()->id,
-                'category_id' =>  $this->category_id,
-                'payment_id'  => null,
-                'qty_teams'   => 1
+                'user_id' => Auth::user()->id,
+                'category_id' => $this->category_id,
+                'payment_id' => null,
+                'qty_teams' => 1,
             ]);
-
         }
 
         $this->create_button_label = __('Create') . ' ' . __('Team');
         $this->store_message(__('Team'));
-	}
+    }
 
     /*+------------------------------+
-	| Lee Registro Editar o Borar  |
-	+------------------------------+
-	 */
+ | Lee Registro Editar o Borar  |
+ +------------------------------+
+ */
 
-	public function edit(Team $record) {
+    public function edit(Team $record)
+    {
         $this->resetInputFields();
         $this->create_button_label = __('Update') . ' ' . __('Team');
-        $this->record       = $record;
-		$this->record_id    = $record->id;
-		$this->name         = $record->name;
-		$this->category_id  = $record->category_id;
-		$this->zipcode      = $record->zipcode;
-        $this->active       = $record->active;
-        $this->enabled       = $record->enabled;
-        $this->town_state   = $record->zipcodex->town . ',' . $record->zipcodex->state;
+        $this->record = $record;
+        $this->record_id = $record->id;
+        $this->name = $record->name;
+        $this->category_id = $record->category_id;
+        $this->zipcode = $record->zipcode;
+        $this->active = $record->active;
+        $this->enabled = $record->enabled;
+        $this->town_state = $record->zipcodex->town . ',' . $record->zipcodex->state;
 
-		$this->openModal();
-	}
+        $this->openModal();
+    }
 
     /*+----------------------------+
-	  | Elimina Registro             |
-	  +------------------------------+
+ | Elimina Registro             |
+ +------------------------------+
     */
-	public function destroy(Team $record) {
+    public function destroy(Team $record)
+    {
         // TODO: Descontar de los equipos por categoría
-        $team_category = TeamCategory::where('category_id',$record->category_id)
-                        ->where('qty_teams','>','registered_teams')
-                        ->where('user_id',Auth::user()->id)
-                        ->whereNull('payment_id')
-                        ->first();
+        $team_category = TeamCategory::where('category_id', $record->category_id)
+            ->where('qty_teams', '>', 'registered_teams')
+            ->where('user_id', Auth::user()->id)
+            ->whereNull('payment_id')
+            ->first();
 
-        if($team_category){
+        if ($team_category) {
             $team_category->delete();
-            $this->delete_record($record,__('Team') . ' ' . __('Deleted Successfully!!'));
+            $this->delete_record($record, __('Team') . ' ' . __('Deleted Successfully!!'));
         }
-
     }
 
     /*+---------------------+
-	  | Lee Zonta Postal    |
-	  +---------------------+
+ | Lee Zonta Postal    |
+ +---------------------+
     */
 
-    public function read_zipcode() {
-
-        $this->town_state =Null;
+    public function read_zipcode()
+    {
+        $this->town_state = null;
         if ($this->zipcode) {
             $zipcode = Zipcode::Zipcode($this->zipcode)->first();
             if ($zipcode) {
@@ -166,15 +177,17 @@ class Teams extends Component {
         }
     }
 
-    public function read_teams() {
-        if($this->category_id){
+    public function read_teams()
+    {
+        if ($this->category_id) {
             $this->category = Category::findOrFail($this->category_id);
             $this->teams = $this->category->teams()->get();
         }
     }
 
-    public function read_players() {
-        if($this->user_id){
+    public function read_players()
+    {
+        if ($this->user_id) {
             $this->team = Team::findOrFail($this->user_id);
             $this->players = $this->team->players()->get();
         }
