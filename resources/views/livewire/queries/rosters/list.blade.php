cdd4f3566611f90692817c63deeaa28d78d59731
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2022-07-16 16:36:54 -0600

    Consulta Rosters: Poner imagen si es niño o niña

--- resources/views/livewire/queries/rosters/list.blade.php
+++ resources/views/livewire/queries/rosters/list.blade.php
@@ -1,12 +1,33 @@
 <tr>
+
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->user->name }}</td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->fullname }}</td>
-    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->birthday }}</td>
-    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ __($record->gender) }}</td>
+    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
+        {{date('M',strtotime($record->birthday)) . '-' .
+        date('d',strtotime($record->birthday))  . '-' .
+        date('Y',strtotime($record->birthday)) }}
+    </td>
+    {{-- <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ __($record->gender) }}</td> --}}
+    <td class="w-25">
+        @if ($record->gender == 'Female')
+            <img src="{{asset('images/girl.jfif')}}"
+                alt="{{ __($record->gender) }}"
+                class="h-15 w-10 rounded-full object-cover"
+            >
+        @else
+            <img src="{{asset('images/boy.jfif')}}"
+                alt="{{ __($record->gender) }}"
+                class="h-15 w-10 rounded-full object-cover text-center"
+            >
+
+        @endif
+
+    </td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
 
         @foreach ($record->teams  as $team)
             {{$team->category->name .'-' . $team->name}}
         @endforeach
     </td>
+
 </tr>
--- resources/views/livewire/queries/rosters/list_by_category.blade.php
+++ resources/views/livewire/queries/rosters/list_by_category.blade.php
@@ -2,8 +2,31 @@
 
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->player->user->name }}</td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->player->fullname }}</td>
-    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ $record->player->birthday }}</td>
-    <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ __($record->player->gender) }}</td>
+
+            <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
+            {{date('M',strtotime($record->player->birthday)) . '-' .
+            date('d',strtotime($record->player->birthday))  . '-' .
+            date('Y',strtotime($record->player->birthday)) }}
+        </td>
+
+
+    {{-- <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">{{ __($record->player->gender) }}</td> --}}
+    <td class="w-25">
+
+        @if ($record->player->gender == 'Female')
+            <img src="{{asset('images/girl.jfif')}}"
+                alt="{{ __($record->player->gender) }}"
+                class="h-15 w-10 rounded-full object-cover text-center"
+            >
+        @else
+            <img src="{{asset('images/boy.jfif')}}"
+                alt="{{ __($record->player->gender) }}"
+                class="h-15 w-10 rounded-full object-cover"
+            >
+
+        @endif
+
+    </td>
     <td class="border px-2 py-1 leading-relaxed sm:text-base md:text-xl xl:text-base text-gray-600">
 
         @foreach ($record->player->teams  as $team)
